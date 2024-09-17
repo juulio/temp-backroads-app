473411b3df1c4c20fe30f86d96662eedd6f29397
Julio Del Valle <juulio@gmail.com>
2024-09-16 21:28:23 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { About } from './components/About'
+import { Hero } from './components/Hero'
+import { Navbar } from './components/Navbar'
+import { Tours } from './components/Tours'
+import { Services } from './components/Services'
+import { Footer } from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  )
+}
+
+export default App
